798e94c38eab9dbb96966e2c8205213b13a4cfdc
davor <davorj@live.com>
2019-05-30 16:32:05 +0000

    roxygen support

--- gwloggeR/R/hello.R
+++ gwloggeR/R/hello.R
@@ -1,18 +1,25 @@
-# Hello, world!
-#
-# This is an example function named 'hello' 
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
-
+#' Hello, world!
+#'
+#' @description
+#' This is an example function named 'hello'
+#' which prints 'Hello, world!'.
+#'
+#' @details
+#' You can learn more about package authoring with RStudio at:
+#'
+#'   http://r-pkgs.had.co.nz/
+#'
+#' Some useful keyboard shortcuts for package authoring:
+#' \tabular{ll}{
+#'    Build and Reload Package:  \tab Ctrl + Shift + B\cr
+#'    Check Package:             \tab Ctrl + Shift + E\cr
+#'    Test Package:              \tab Ctrl + Shift + T\cr
+#'    Roxygen:                   \tab Ctrl + Shift + D\cr
+#' }
+#'
+#' @examples
+#' hello()
+#'
 hello <- function() {
   print("Hello, world!")
 }
