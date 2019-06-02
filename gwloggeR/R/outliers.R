aeabd976966c6c4af5a6c0ef39884ff93f895369
Davor Josipovic <davorj@live.com>
2019-06-02 12:41:01 +0000

    S4 detect_outliers generic method that dispatches submethods, based on whether apriori information is provided

--- gwloggeR/R/outliers.R
+++ gwloggeR/R/outliers.R
@@ -2,7 +2,7 @@
 #' @description
 #' This function marks outliers in the input vector.
 #' @param x numeric vector of values
-#' @return logical vector with same length as x, specifying TRUE for outlier
+#' @return Logical vector with same length as x, specifying TRUE for an outlier.
 #' @examples
 #' # In case of a vector:
 #' x <- c(1:10, 100)
@@ -17,7 +17,27 @@
 #'
 #' @export
 #' @rdname detect_outliers
+setGeneric("detect_outliers",
+           signature = c("x", "apriori"),
+           valueClass = "logical",
+           function(x, apriori) standardGeneric('detect_outliers')
+)
 
-detect_outliers <- function(x) {
-  detect_outliers_leys(x)
-}
+#' @describeIn detect_outliers
+#' Only considers x, without any _a-priori_ information.
+#' A normal distribution is assumed with mean and variance estimated using
+#' median and MAD as described in Leys, 2013.
+#' @references Leys, C. e.a., Detecting outliers, 2013.
+setMethod('detect_outliers',
+          signature = c(x = "numeric", apriori = "missing"),
+          function(x) {
+  detect_outliers_norm(x, x.mean = median(x, na.rm = TRUE), x.sd = mad(x, na.rm = TRUE))
+})
+
+#' @describeIn detect_outliers
+#' Takes _a-priori_ data information into consideration.
+setMethod('detect_outliers',
+          signature = c(x = "numeric", apriori = "apriori"),
+          function(x, apriori) {
+  detect_outliers_norm(x, x.mean = apriori$mean, x.sd = sqrt(apriori$var))
+})
