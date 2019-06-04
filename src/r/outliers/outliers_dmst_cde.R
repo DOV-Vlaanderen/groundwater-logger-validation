84c6dfc6956e649ed8cd85d25ab4277426b06170
davorj <davorj@live.com>
2019-06-04 22:09:27 +0200

    refactor

--- src/r/outliers/outliers_dmst_cde.R
+++ src/r/outliers/outliers_dmst_cde.R
@@ -1,4 +1,4 @@
-source('outliers/outliers_dmst_cde.plot.R')
+source('outliers/outliers.plot.R')
 
 # ENT (entered) is the basic status meaning that these measurements are used
 # in the workflow; DEL (deleted) and INV (invalid) are measurements that were
@@ -9,8 +9,14 @@ local({
   folder <- "./../../data/raw/inbo/"
   pdf(file = './outliers/outliers_dmst_cde.pdf', width = 14, height = 7, compress = FALSE)
   for (f in list.files(folder, full.names = TRUE, pattern = ".*\\.csv")) {
-    print(plot.dmst_cde(f))
+    df_raw <- data.table::fread(f, dec = ",")
+    df_raw[, DRME_OCR_UTC_DTE := as.POSIXct(gsub("(.*):", "\\1", DRME_OCR_UTC_DTE),
+                                            format = "%d/%m/%Y %H:%M:%S %z", tz = 'UTC')]
+
+    print(with(df_raw, plot.dmst_cde(x = DRME_OCR_UTC_DTE,
+                                     y = DRME_DRU,
+                                     dmst_cde = DRME_DMST_CDE,
+                                     title = basename(f))))
   }
   dev.off()
 })
-
