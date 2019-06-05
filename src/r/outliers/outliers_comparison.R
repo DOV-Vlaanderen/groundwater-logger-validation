d6b215bd14697294c695ae03765083d855fcadbd
davorj <davorj@live.com>
2019-06-05 23:05:59 +0200

    v0.02 added

--- src/r/outliers/outliers_comparison.R
+++ src/r/outliers/outliers_comparison.R
@@ -10,6 +10,13 @@ local({
     df[, DRME_OCR_UTC_DTE := as.POSIXct(gsub("(.*):", "\\1", DRME_OCR_UTC_DTE),
                                         format = "%d/%m/%Y %H:%M:%S %z", tz = 'UTC')]
 
+    point_sample_type <- substr(basename(f), 4L, 4L)
+    ap <- switch (point_sample_type,
+                  "L" = apriori("air pressure", "cmH2O"),
+                  "P" = apriori("diver", "cmH2O"),
+                  "S" = apriori("diver", "cmH2O")
+    )
+
     p_dmst_cde <- with(df, plot.dmst_cde(x = DRME_OCR_UTC_DTE,
                                          y = DRME_DRU,
                                          dmst_cde = DRME_DMST_CDE,
@@ -20,7 +27,12 @@ local({
                                       outliers = detect_outliers(DRME_DRU),
                                       title = paste(basename(f), "(v0.01)")))
 
-    p <- gridExtra::grid.arrange(p_dmst_cde, p_v0.01)
+    p_v0.02 <- with(df, plot.outliers(x = DRME_OCR_UTC_DTE,
+                                      y = DRME_DRU,
+                                      outliers = detect_outliers(DRME_DRU, apriori = ap),
+                                      title = paste(basename(f), "(v0.02)")))
+
+    p <- gridExtra::grid.arrange(p_dmst_cde, p_v0.01, p_v0.02)
     print(p)
   }
   dev.off()
