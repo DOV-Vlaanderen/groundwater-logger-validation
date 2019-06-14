4e578b27eefec4ebf258ed95d3486e9ffb533ae2
Davor Josipovic <davorj@live.com>
2019-06-14 19:57:54 +0200

    uncompressed pdf

--- src/r/outliers/outliers_v0.01.R
+++ src/r/outliers/outliers_v0.01.R
@@ -4,8 +4,9 @@ source("outliers/outliers.plot.R")
 
 local({
   folder <- "./../../data/raw/inbo/"
-  pdf(file = './outliers/outliers_v0.01.pdf', width = 14, height = 7, compress = TRUE)
+  pdf(file = './outliers/outliers_v0.01.pdf', width = 14, height = 7, compress = FALSE)
   for (f in list.files(folder, full.names = TRUE, pattern = ".*\\.csv")) {
+    print(basename(f))
     df_raw <- data.table::fread(f, dec = ",")
     df_raw[, DRME_OCR_UTC_DTE := as.POSIXct(gsub("(.*):", "\\1", DRME_OCR_UTC_DTE),
                                             format = "%d/%m/%Y %H:%M:%S %z", tz = 'UTC')]
