ea284ed9dcb128054f36217f82c5e0b3681d1c4a
davorj <davorj@live.com>
2019-07-07 20:23:26 +0000

    predefined structure is now returned by detect_outliers_norm()

--- gwloggeR/R/outliers_norm.R
+++ gwloggeR/R/outliers_norm.R
@@ -4,14 +4,20 @@
 detect_outliers_norm <- function(x, p.value = 0.0005, verbose = FALSE, x.mean, x.sd,
                                  type = c("two.sided", "less", "greater")) {
   type <- match.arg(type)
+  N <- length(x)
+
+  return.obj <- function(x.rejects, sigma.reject = NULL) {
+    structure(x.rejects, "x.mean" = x.mean, "x.sd" = x.sd,
+              "p.value" = p.value, "type" = type,
+              "sigma.reject" = sigma.reject)
+  }
 
   if (is.na(x.sd) | x.sd == 0) {
-    return(rep(FALSE, length(x)))
+    return(return.obj(rep(FALSE, N)))
   }
 
   # sigma.reject is the sigma after which we reject points. The 0.0005 means that
   # in 1 of 2000 (1/0.0005) calculations, we will reject a value we shouldn't have.
-  N <- length(x)
   TS <- if (type == "two.sided") 2 else 1
   sigma.reject <- qnorm(p = (1 - p.value)^(1 / (N*TS)))
   x.norm <- (x - x.mean) / x.sd
@@ -30,5 +36,5 @@ detect_outliers_norm <- function(x, p.value = 0.0005, verbose = FALSE, x.mean, x
     ))
   }
 
-  x.rejects
+  return.obj(x.rejects, sigma.reject = sigma.reject)
 }
