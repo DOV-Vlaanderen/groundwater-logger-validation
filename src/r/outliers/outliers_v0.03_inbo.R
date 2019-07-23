e94a377e1993bfeed5f47ccaca581bd1c9c4f59a
davorj <davorj@live.com>
2019-07-23 17:57:35 +0200

    outliers v0.03 for inbo

---
+++ src/r/outliers/outliers_v0.03_inbo.R
@@ -0,0 +1,25 @@
+library(gwloggeR)
+
+local({
+  print(Sys.time())
+  for (f in Logger::enumerate(partner = 'inbo')) {
+    print(basename(f))
+    df <- Logger(f)$df
+
+    point_sample_type <- substr(basename(f), 4L, 4L)
+    ap <- switch (point_sample_type,
+                  "L" = apriori("air pressure", "cmH2O"),
+                  "P" = apriori("hydrostatic pressure", "cmH2O"),
+                  "S" = apriori("hydrostatic pressure", "cmH2O")
+    )
+
+    local({
+      png(paste0('./outliers/outliers_v0.03_inbo/', basename(f), '.png'), width = 1920, height = 1080)
+      on.exit(dev.off())
+      detect_outliers(df$PRESSURE_VALUE, apriori = ap,
+                      timestamps = if (!all(is.na(df$TIMESTAMP_UTC))) df$TIMESTAMP_UTC,
+                      plot = TRUE, title = paste0(basename(f), ' - v0.02'))
+    })
+  }
+  print(Sys.time())
+})
