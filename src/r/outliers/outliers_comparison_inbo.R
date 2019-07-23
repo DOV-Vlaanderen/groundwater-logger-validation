dbb1c8380631a23f2d3c5c39b5c37f6d10b8931b
davorj <davorj@live.com>
2019-07-23 21:07:01 +0200

    comparison based on existing png's

--- src/r/outliers/outliers_comparison_inbo.R
+++ src/r/outliers/outliers_comparison_inbo.R
@@ -1,39 +1,17 @@
-source("outliers/outliers.plot.R")
-
-library(gwloggeR)
-
 local({
-  folder <- "./../../data/raw/inbo/"
-  pdf(file = './outliers/outliers_comparison.pdf', width = 14, height = 7, compress = TRUE)
-  for (f in list.files(folder, full.names = TRUE, pattern = ".*\\.csv")) {
-    df <- data.table::fread(f, dec = ",")
-    df[, DRME_OCR_UTC_DTE := as.POSIXct(gsub("(.*):", "\\1", DRME_OCR_UTC_DTE),
-                                        format = "%d/%m/%Y %H:%M:%S %z", tz = 'UTC')]
+  print(Sys.time())
+  folder <- c("./../r/outliers/outliers_v0.01_inbo/",
+              "./../r/outliers/outliers_v0.02_inbo/",
+              "./../r/outliers/outliers_v0.03_inbo/")
+  pdf(file = './outliers/outliers_comparison_inbo.pdf', width = 16, height = 9, compress = TRUE)
+  on.exit(dev.off())
 
-    point_sample_type <- substr(basename(f), 4L, 4L)
-    ap <- switch (point_sample_type,
-                  "L" = apriori("air pressure", "cmH2O"),
-                  "P" = apriori("diver", "cmH2O"),
-                  "S" = apriori("diver", "cmH2O")
-    )
+  for (f in Logger::enumerate('inbo')) {
+    print(basename(f))
 
-    p_dmst_cde <- with(df, plot.dmst_cde(x = DRME_OCR_UTC_DTE,
-                                         y = DRME_DRU,
-                                         dmst_cde = DRME_DMST_CDE,
-                                         title = basename(f)))
-
-    p_v0.01 <- with(df, plot.outliers(x = DRME_OCR_UTC_DTE,
-                                      y = DRME_DRU,
-                                      outliers = detect_outliers(DRME_DRU),
-                                      title = paste(basename(f), "(v0.01)")))
-
-    p_v0.02 <- with(df, plot.outliers(x = DRME_OCR_UTC_DTE,
-                                      y = DRME_DRU,
-                                      outliers = detect_outliers(DRME_DRU, apriori = ap),
-                                      title = paste(basename(f), "(v0.02)")))
-
-    p <- gridExtra::grid.arrange(p_dmst_cde, p_v0.01, p_v0.02)
-    print(p)
+    file.png <- list.files(folder, pattern = basename(f), ignore.case = TRUE, recursive = TRUE, full.names = TRUE)
+    png.list <- lapply(file.png, function(fn) grid::rasterGrob(png::readPNG(fn)))
+    do.call(what = gridExtra::grid.arrange, args = c(png.list, list('ncol' = 2)))
   }
-  dev.off()
+  print(Sys.time())
 })
