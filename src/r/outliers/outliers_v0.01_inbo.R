8e9ff52d626adf9f3695111f474fa3cce7d16f48
davorj <davorj@live.com>
2019-07-23 16:58:45 +0200

    added version in title

--- src/r/outliers/outliers_v0.01_inbo.R
+++ src/r/outliers/outliers_v0.01_inbo.R
@@ -10,7 +10,7 @@ local({
       png(paste0('./outliers/outliers_v0.01_inbo/', basename(f), '.png'), width = 1920, height = 1080)
       on.exit(dev.off())
       detect_outliers(df$PRESSURE_VALUE, timestamps = if (!all(is.na(df$TIMESTAMP_UTC))) df$TIMESTAMP_UTC,
-                      plot = TRUE, title = basename(f))
+                      plot = TRUE, title = paste0(basename(f), ' - v0.01'))
     })
   }
   print(Sys.time())
