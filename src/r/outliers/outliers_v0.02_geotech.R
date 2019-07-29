990c2efcaa7e191c512498ea5f318ec373a60782
davorj <davorj@live.com>
2019-07-29 21:18:22 +0200

    lowered png resolution

--- src/r/outliers/outliers_v0.02_geotech.R
+++ src/r/outliers/outliers_v0.02_geotech.R
@@ -7,7 +7,7 @@ local({
     df <- Logger(f)$df
 
     local({
-      png(paste0('./outliers/outliers_v0.02_geotech/', basename(f), '.png'), width = 1920, height = 1080)
+      png(paste0('./outliers/outliers_v0.02_geotech/', basename(f), '.png'), width = 1280, height = 720)
       on.exit(dev.off())
       detect_outliers(df$PRESSURE_VALUE, timestamps = df$TIMESTAMP_UTC, plot = TRUE, title = f,
                       apriori = apriori("hydrostatic pressure"))
