e886d01633614c334d1ca222542dc915c4c8c244
Davor Josipovic <davorj@live.com>
2019-08-17 18:01:58 +0000

    differenceplot

--- gwloggeR/R/levelshifts_plot.R
+++ gwloggeR/R/levelshifts_plot.R
@@ -20,8 +20,24 @@ scatterplot.levelshifts <- function(x, levelshifts = rep(FALSE, length(x)), time
     ggplot2::theme_light()
 }
 
+differenceplot <- function(x, timestamps = NULL) {
+  xdiff <- c(NA, diff(x))
+  n <- length(x)
+  timestamps.invalid <- is.null(timestamps) | all(is.na(timestamps))
+  x.axis <- if (timestamps.invalid) 1:n else timestamps
+  data <- data.frame(x = x.axis, y = xdiff, outliers = FALSE)[order(x.axis),]
+
+  ggplot2::ggplot(data = data, mapping = ggplot2::aes_string(x = "x", y = "y")) +
+    ggplot2::geom_line(na.rm = TRUE) +
+    ggplot2::geom_point(mapping = ggplot2::aes_string(color = "outliers"), show.legend = FALSE, na.rm = TRUE) +
+    ggplot2::scale_color_manual(name = "OUTLIER", values = c("FALSE" = "black", "TRUE" = "red")) +
+    ggplot2::ylab('x') + ggplot2::xlab(if (timestamps.invalid) 'sequence' else 'timestamp') +
+    ggplot2::theme_light()
+}
+
 #set.seed(2020)
 #print(scatterplot.levelshifts(rnorm(100), rep(c(FALSE, TRUE, FALSE, TRUE, FALSE), c(10, 1, 59, 1, 29)),
 #      seq(Sys.time(), Sys.time() + 10, length.out = 100)))
 #print(scatterplot.levelshifts(rnorm(100), rep(c(FALSE, TRUE, FALSE, TRUE, FALSE), c(10, 1, 59, 1, 29))))
 #print(scatterplot.levelshifts(rnorm(100)))
+#print(differenceplot(rnorm(100), timestamps = seq(Sys.time(), Sys.time() + 24*60*60, length.out = 100)))
