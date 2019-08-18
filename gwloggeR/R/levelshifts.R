b23f4bb7ecd2f0b1b2dbccbc914bacb1ae24e9a5
Davor Josipovic <davorj@live.com>
2019-08-18 19:15:38 +0000

    levelshift template

---
+++ gwloggeR/R/levelshifts.R
@@ -0,0 +1,8 @@
+#' @title Levelshifts object
+#' @description Levelshifts object holds all the necessary information about detected levelshifts.
+#' @keywords internal
+
+Levelshifts <- function() {
+  structure()
+}
+
