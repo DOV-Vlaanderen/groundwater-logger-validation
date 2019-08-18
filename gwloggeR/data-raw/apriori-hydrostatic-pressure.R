b5ceeaec98dd3b5eba4bb84ffea9c16cc5d094ab
Davor Josipovic <davorj@live.com>
2019-08-18 19:12:20 +0000

    apriori hydrostatic pressure data

---
+++ gwloggeR/data-raw/apriori-hydrostatic-pressure.R
@@ -0,0 +1,61 @@
+## code to prepare `apriori-hydrostatic-pressure` dataset goes here
+
+options(logger.root.data.path = './../data/raw')
+devtools::load_all('./../gwloggeR.data', export_all = FALSE)
+detach('package:gwloggeR.data')
+
+logger_selection <- c('born_pb 112o_151202090851_T4179',
+                      'born_pb94d_170124133301_T4175',
+                      'born_pb94d_171009123814_T4175',
+                      'born_pb94d_171213132622_T4175',
+                      'born_pb94d_180213134811_T4175',
+                      'born_pb94d_180315155306_T4175',
+                      'born_pb94o_170403160106_T4184',
+                      'born_pb94o_170623135603_T4184',
+                      'born_pb94o_170727122028_T4184',
+                      'born_pb94o_171009123814_T4184',
+                      'born_pb94o_171213132623_T4184',
+                      'born_pb94o_180111133149_T4184',
+                      'born_pb94o_180315155143_T4184',
+                      'born_pb112d_171009123816_T4180',
+                      'born_pb112d_180111134538_T4180',
+                      'born_pb112d_180213134811_T4180',
+                      'born_pb112d_180315160037_T4180',
+                      'born_pb112o_170623134158_T4179',
+                      'born_pb112o_170727122029_T4179',
+                      'born_pb112o_170829130424_T4179',
+                      'born_pb112o_171009123815_T4179',
+                      'born_pb112o_171213132623_T4179',
+                      'born_pb112o_180213134812_T4179',
+                      'ff_b29d_090429160100_C4080',
+                      'ff_b51d_090305163025_C4089',
+                      'pp01-1_101104155528_F5468',
+                      'pp09-1_101104150356_F5474',
+                      'pp09-2_101104150902_F8511')
+
+hydropressure <- lapply(logger_selection, function(file.name) {
+  df <- gwloggeR.data::read(file.name)$df
+
+  # No NA timestamps allowed
+  if (any(is.na(df[, TIMESTAMP_UTC])))
+    stop('ERROR: all timestamps must be non-NA.')
+
+  # Equal spacing is not a requirement, but it is
+  # exploited to speed up sample generation of differences.
+  if (length(unique(as.numeric(diff(df[, TIMESTAMP_UTC]), 'secs'))) != 1L)
+    stop('ERROR: all differences must be equaly spaced.')
+
+  # Meta data
+  df[, FILE := basename(file.name)]
+  df[, N := .N]
+
+  data.table::setkey(df, TIMESTAMP_UTC)
+
+  df
+})
+
+# intervals.sec table
+table(sapply(hydropressure, function(df) unique(as.numeric(diff(df$TIMESTAMP_UTC), units = 'secs'))))
+
+hydropressure <- data.table::rbindlist(hydropressure, use.names = TRUE)
+data.table::setkey(hydropressure, FILE, TIMESTAMP_UTC)
