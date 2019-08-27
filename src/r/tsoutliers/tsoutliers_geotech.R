442ac7abc09f7ab8fa2fa4675387deba86e0cc65
Davor Josipovic <davorj@live.com>
2019-08-27 22:27:34 +0200

    small update to use gwloggeR.data and error message

--- src/r/tsoutliers/tsoutliers_geotech.R
+++ src/r/tsoutliers/tsoutliers_geotech.R
@@ -6,14 +6,14 @@ local({
   fn <- function(f) {
     print(Sys.time())
     print(basename(f))
-    df <- Logger(f)$df
+    df <- gwloggeR.data::read(f)$df
 
     # remove no-timestamp
     df <- df[!is.na(TIMESTAMP_UTC),]
     data.table::setkey(df, TIMESTAMP_UTC)
 
     N <- nrow(df)
-    if (N == 0L) next()
+    if (N == 0L) return()
 
     y <- ts(gwloggeR:::aggregate(x = df[,PRESSURE_VALUE], ts = df[, TIMESTAMP_UTC], by = 'hours'))
     tso <- try(tsoutliers::tso(y = y
@@ -31,14 +31,14 @@ local({
       png(paste0('./tsoutliers/tsoutliers_geotech/', basename(f), '.png'), width = 1280, height = 720)
       on.exit(dev.off())
       if (!error && length(tso$outliers$ind) > 0L) plot(tso) else plot.new()
-      mtext(paste0(if (error) geterrmessage(),
+      mtext(paste0(if (error) attr(tso, 'condition')$message,
                    basename(f), ' - ',
                    if (!error) forecast:::arima.string(tso$fit) else '/',
                    ', N = ', length(y)), side = 3, line = -2, outer = TRUE)
     })
   }
 
-  parallel::clusterApplyLB(cl = cl, x = Logger::enumerate(partner = 'geotech'), fun = fn)
+  parallel::clusterApplyLB(cl = cl, x = gwloggeR.data::enumerate(partner = 'geotech'), fun = fn)
 
   print(Sys.time())
 })
