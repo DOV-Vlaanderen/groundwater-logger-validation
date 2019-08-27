1dcd3dbeba5b9f7502776e75b5fb01a3033e7fa9
davorj <davorj@live.com>
2019-08-27 21:43:55 +0200

    error cases with no data + reran everything

--- src/r/tsoutliers/tsoutliers_inbo.R
+++ src/r/tsoutliers/tsoutliers_inbo.R
@@ -1,9 +1,12 @@
 local({
   print(Sys.time())
+  root.dir <- './tsoutliers/tsoutliers_inbo'
   cl <- parallel::makeCluster(7, outfile = './log/parallel.log')
   on.exit(parallel::stopCluster(cl))
 
   fn <- function(f) {
+    loadNamespace('tsoutliers')
+
     print(Sys.time())
     print(basename(f))
     df <- gwloggeR.data::read(f)$df
@@ -13,30 +16,38 @@ local({
     data.table::setkey(df, TIMESTAMP_UTC)
 
     N <- nrow(df)
-    if (N == 0L) next()
 
-    y <- ts(gwloggeR:::aggregate(x = df[,PRESSURE_VALUE], ts = df[, TIMESTAMP_UTC], by = 'days'))
-    tso <- tsoutliers::tso(y = y
-                           #, types = 'LS'
-                           #, discard.method = 'bottom-up'
-                           , cval = gwloggeR:::c.norm.optimal(alpha = 0.0005, n = N, type = 'two.sided')
-                           #,tsmethod = 'arima'
-                           #, args.tsmethod = list(order = c(0, 1, 0), seasonal = list(order = c(0, 0, 0)))
-                           )
+    y <- if (N != 0L) ts(gwloggeR:::aggregate(x = df[,PRESSURE_VALUE], ts = df[, TIMESTAMP_UTC], by = 'days'))
 
-    saveRDS(tso, paste0('./tsoutliers/tsoutliers_inbo/', basename(f), '.tsoutliers'), ascii = TRUE, compress = FALSE)
+    tsoutliers.file <- paste0(root.dir, '/', basename(f), '.tsoutliers')
+    if (!file.exists(tsoutliers.file)) {
+      tso <- try(tsoutliers::tso(y = y
+                                 #, types = 'LS'
+                                 #, discard.method = 'bottom-up'
+                                 , cval = gwloggeR:::c.norm.optimal(alpha = 0.0005, n = N, type = 'two.sided')
+                                 #,tsmethod = 'arima'
+                                 #, args.tsmethod = list(order = c(0, 1, 0), seasonal = list(order = c(0, 0, 0)))
+      ))
+
+      saveRDS(tso, file = tsoutliers.file, ascii = TRUE, compress = FALSE)
+    }
+
+    tso <- readRDS(tsoutliers.file)
+    error <- inherits(tso, 'try-error')
 
     local({
-      png(paste0('./tsoutliers/tsoutliers_inbo/', basename(f), '.png'), width = 1280, height = 720)
+      png(paste0(root.dir, '/', basename(f), '.png'), width = 1280, height = 720)
       on.exit(dev.off())
-      if (length(tso$outliers$ind) > 0L) plot(tso) else plot.new()
-      mtext(paste0(basename(f), ' - ', forecast:::arima.string(tso$fit), ', N = ', length(y)), side = 3, line = -2, outer = TRUE)
+      if (!error && length(tso$outliers$ind) > 0L) plot(tso) else plot.new()
+      mtext(paste0(if (error) attr(tso, 'condition')$message,
+                   basename(f), ' - ',
+                   if (!error) forecast:::arima.string(tso$fit) else '/',
+                   ', N = ', length(y)), side = 3, line = -2, outer = TRUE)
     })
-
   }
 
   parallel::clusterApplyLB(cl = cl, x = gwloggeR.data::enumerate(partner = 'inbo'), fun = fn)
+  #lapply(gwloggeR.data::enumerate(partner = 'inbo'), fn)
 
   print(Sys.time())
 })
-
