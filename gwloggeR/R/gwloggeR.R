d59a71de691d45fde5a51c67f2d988243414b00e
davorj <davorj@live.com>
2019-09-11 19:58:23 +0000

    NSE R CMD check notes...

--- gwloggeR/R/gwloggeR.R
+++ gwloggeR/R/gwloggeR.R
@@ -9,3 +9,11 @@
 #' @references \href{https://github.com/DOV-Vlaanderen/groundwater-logger-validation/tree/master/gwloggeR}{GitHub}
 "_PACKAGE"
 #> [1] "_PACKAGE"
+
+# NSE variables that cause CRAN R CMD check notes...
+# Very ugly, but the solution is suggested by CRAN
+# See: https://stackoverflow.com/q/9439256/1548942
+utils::globalVariables(c('J', '.', '.N')) # data.table internals
+utils::globalVariables(c('.valueClassTest')) # S4 without full methods import
+utils::globalVariables(c('TIMESTAMP_UTC', 'FILE', 'PRESSURE_VALUE'))
+utils::globalVariables(c('DIFF.SEC', 'CUM.DIFF.SEC', 'DIFF.VALUE'))
