b1e08a102d5ca95b15c924f9b7df55c9e2076b99
davorj <davorj@live.com>
2019-09-12 19:09:41 +0000

    detect_levelshifts cleanup

--- gwloggeR/R/levelshifts.R
+++ gwloggeR/R/levelshifts.R
@@ -1,7 +1,7 @@
 #' @title Levelshifts object
 #' @description Levelshifts object holds all the necessary information about detected levelshifts.
 #' @keywords internal
-
+#'
 Levelshifts <- function(vec) {
   structure(vec)
 }
@@ -39,28 +39,20 @@ setMethod(
   signature = c(x = "numeric", apriori = "apriori"),
   function(x, timestamps, apriori, plot, verbose, title) {
 
-    if (!is.null(timestamps)) {
-      assert.timestamp(timestamps)
-      validate.timestamp(timestamps)
-      if (length(timestamps) != length(x)) stop('x and timestamps must have same length.')
-    }
+    if (apriori$data_type == "air pressure") return ({
+      warning('WARNING: levelshifts for air pressure data not implemented.')
+      Levelshifts(rep(FALSE, length(x)))
+    })
 
     if (apriori$data_type == "hydrostatic pressure") return ({
+
+      hydropressure.timestamp.validation(timestamps)
+
       det <- detect(x = x, timestamps = timestamps)
       ls.x <- rep(FALSE, length(x))
       ls.x[det[type == 'LS', index]] <- TRUE
 
       levelshifts <- Levelshifts(ls.x)
-
-      if (plot) {
-        d <- differenceplot(x, timestamps = timestamps, outliers = levelshifts)
-        s <- scatterplot.levelshifts(x, levelshifts = levelshifts, timestamps = timestamps)
-        layout_matrix <- rbind(c(1,1),
-                               c(2,2))
-        grob.title <- if (!is.null(title)) grid::textGrob(title, x = 0.05, hjust = 0)
-        gridExtra::grid.arrange(s, d, layout_matrix = layout_matrix,
-                                top = grob.title)
-      }
 
       if (verbose) levelshifts else as.vector(levelshifts)
     })
