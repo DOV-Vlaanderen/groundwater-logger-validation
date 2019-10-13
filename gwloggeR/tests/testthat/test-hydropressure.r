f3608558d4db7a6c5be92844e19ebb55e0de22c7
davorj <davorj@live.com>
2019-10-13 19:21:43 +0000

    file rename

--- gwloggeR/tests/testthat/test-hydropressure.r
+++ gwloggeR/tests/testthat/test-hydropressure.r
@@ -7,8 +7,8 @@ x <- c(-50, rnorm(18), 50, rnorm(19) + 50, -50, rnorm(4), rnorm(45) + 100*0.8^(0
 timestamps <- seq(as.POSIXct('2000-01-01'), length.out = length(x), by = '15 min')
 # plot(x)
 
-# saveRDS(detect(x = x, timestamps = timestamps)[order(index, type)], file = './tests/testthat/test-hydropressure.rds')
-res.expected <- readRDS('test-hydropressure.rds')
+# saveRDS(detect(x = x, timestamps = timestamps)[order(index, type)], file = './tests/testthat/test-hydropressure-detect.rds')
+res.expected <- readRDS('test-hydropressure-detect.rds')
 res.expected
 
 testthat::test_that('Hydrostatic pressure detect function finds all the events.', {
