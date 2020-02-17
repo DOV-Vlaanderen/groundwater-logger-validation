820a500389ddc0592a044dd29bf97a0c907132da
davorj <davorj@live.com>
2020-02-17 20:38:21 +0000

    v04 of drift analysis

--- src/r/drifts/analysis_04.r
+++ src/r/drifts/analysis_04.r
@@ -1,55 +1,42 @@
 # Analysis of differences between one barometer and many others in its neighbourhood.
 # This one is the same as v02, but uses better matching of timestamps.
 
-locations <- read.csv('./../../data/meta/inbo/Baro_Position_Lambert72.csv',
-                      dec = ',', sep = ';', na.strings = 'NULL', stringsAsFactors = FALSE)
-data.table::setDT(locations)
-data.table::setkey(locations, ppnt_cde)
+logger.names <- grep('barodata/', gwloggeR.data::enumerate(), value = TRUE)
 
-d <- function(xy1, xy2) sqrt(sum((xy1 - xy2)^2))
-d.vec <- function(xy.from, xy.to.df) {
-  stopifnot(length(xy.from) == 2L)
-  stopifnot(!is.null(nrow(xy.to.df)))
-
-  apply(xy.to.df, 1, d, xy1 = xy.from)
+round_timestamp <- function(ts, scalefactor.sec = 3600*12) {
+  as.POSIXct(round(as.numeric(ts)/scalefactor.sec) * scalefactor.sec, origin = '1970-01-01', tz = 'UTC')
 }
 
-read.full <- function(location) {
-  loggers <- grep(paste0('barodata/', location), gwloggeR.data::enumerate(), value = TRUE)
-  df <- data.table::rbindlist(lapply(loggers, function(logger) gwloggeR.data::read(logger)$df), use.names = TRUE)
+read.baro <- function(logger.name) {
+  df <- gwloggeR.data::read(logger.name)$df
   if (nrow(df) == 0L) return(df)
+  df <- df[!is.na(TIMESTAMP_UTC), ]
+  df <- df[!is.na(PRESSURE_VALUE), ]
   df <- df[!duplicated(TIMESTAMP_UTC), ]
+  df <- df[, .('PRESSURE_VALUE' = mean(PRESSURE_VALUE)),
+           by = .('TIMESTAMP_UTC' = round_timestamp(TIMESTAMP_UTC))]
   data.table::setkey(df, TIMESTAMP_UTC)
-  structure(df, 'location' = location, 'loggers' = basename(loggers))
+  structure(df, 'logger.name' = logger.name)
 }
 
-compare <- function(df1, df2, distance = NA) {
+# read.baro('barodata/BAOL553X_B_004BA.csv')
+
+compare <- function(df1, df2) {
   if (nrow(df1) == 0L || nrow(df2) == 0L) return(data.table::data.table())
-  df1.name <- attr(df1, 'location')
-  df2.name <- attr(df2, 'location')
 
   diff.df <- df1[J(df2), .('PRESSURE_DIFF' = x.PRESSURE_VALUE - i.PRESSURE_VALUE, TIMESTAMP_UTC)][!is.na(PRESSURE_DIFF), ]
-  if (nrow(diff.df) < 250L) return(data.table::data.table())
-
-  ap <- gwloggeR::apriori('air pressure')
-  df1.operc <- sum(gwloggeR::detect_outliers(df1[J(diff.df), PRESSURE_VALUE], apriori = ap))/nrow(diff.df)
-  df2.operc <- sum(gwloggeR::detect_outliers(df2[J(diff.df), PRESSURE_VALUE], apriori = ap))/nrow(diff.df)
-  if (df1.operc > 0.1 || df2.operc > 0.1) return(data.table::data.table())
 
   diff.df
 }
 
-compute <- function(logger.loc, nr_surrounding_barros = length(distances)) {
-  logger.xy <- unlist(locations[J(logger.loc), .(Lambert72_X, Lambert72_Y)])
-  logger.df <- read.full(logger.loc)
 
-  distances <- d.vec(logger.xy, locations[, .(Lambert72_X, Lambert72_Y)])/1000
-  names(distances) <- locations$ppnt_cde
-  distances <- sort(distances, na.last = TRUE)[-1L]
+compute <- function(logger.name) {
+  logger.names <- setdiff(logger.names, logger.name)
+  logger.df <- read.baro(logger.name)
 
-  df.diff <- lapply(names(distances[1L:nr_surrounding_barros]), function(other.loc) {
-    other.df <- read.full(other.loc)
-    compare(logger.df, other.df, distances[other.loc])
+  df.diff <- lapply(logger.names, function(other.name) {
+    other.df <- read.baro(other.name)
+    compare(logger.df, other.df)
   })
   df.diff <- data.table::rbindlist(df.diff, use.names = TRUE, fill = TRUE)
 
@@ -61,31 +48,47 @@ compute <- function(logger.loc, nr_surrounding_barros = length(distances)) {
         Q.975 = quantile(PRESSURE_DIFF, 0.975)),
     by = TIMESTAMP_UTC]
 
-  plt <- ggplot2::ggplot(data = df.diff, mapping = ggplot2::aes(x = TIMESTAMP_UTC, y = PRESSURE_DIFF)) +
+  plt.logger <- ggplot2::ggplot(data = logger.df, mapping = ggplot2::aes(x = TIMESTAMP_UTC, y = PRESSURE_VALUE)) +
+    ggplot2::geom_line() +
+    ggplot2::coord_cartesian(ylim = quantile(logger.df$PRESSURE_VALUE, probs = c(0.005, 0.995)),
+                             xlim = range(logger.df$TIMESTAMP_UTC)) +
+    ggplot2::theme_light() +
+    ggplot2::theme(axis.title.x = ggplot2::element_blank(),
+                   axis.text.x = ggplot2::element_blank(),
+                   axis.text.y = ggplot2::element_text(angle = 90, hjust = 0.5))
+
+  plt.diff <- ggplot2::ggplot(data = df.diff, mapping = ggplot2::aes(x = TIMESTAMP_UTC, y = PRESSURE_DIFF)) +
     ggplot2::geom_point(pch = '.') +
     ggplot2::geom_line(data = df.quant, mapping = ggplot2::aes(y = Q.5), col = 'red', alpha = 0.5) +
     ggplot2::stat_smooth(method = 'gam', formula = y ~ s(x, k = 40), color = 'yellow', se = FALSE) +
-    ggplot2::coord_cartesian(ylim = quantile(df.diff$PRESSURE_DIFF, probs = c(0.025, 0.975))) +
-    ggplot2::scale_x_datetime(date_breaks = '1 year', date_labels = '%Y') +
-    ggplot2::ggtitle(sprintf('%s (%s) #%s', logger.loc, paste0(attr(logger.df, 'loggers'), collapse = ', '), nrow(df.diff))) +
-    ggplot2::theme_light()
+    ggplot2::coord_cartesian(ylim = quantile(df.diff$PRESSURE_DIFF, probs = c(0.025, 0.975)),
+                             xlim = range(logger.df$TIMESTAMP_UTC)) +
+    #ggplot2::scale_x_datetime(date_breaks = '1 year', date_labels = '%Y') +
+    ggplot2::theme_light() +
+    ggplot2::theme(axis.text.y = ggplot2::element_text(angle = 90, hjust = 0.5))
 
-  filename <- sprintf('./drifts/analysis_04/%s.png', logger.loc)
+  layout_matrix <- rbind(1,2)
+  grob.title <- grid::textGrob(sprintf('%s (#%s vs. #%s observations)',
+                                       basename(logger.name), nrow(logger.df), nrow(df.diff)),
+                               x = 0.05, hjust = 0)
+
+  filename <- sprintf('./drifts/analysis_04/%s.png', tools::file_path_sans_ext(basename(logger.name)))
 
   local({
     png(filename, width = 1280, height = 720)
     on.exit(dev.off())
-    print(plt)
+    print(gridExtra::grid.arrange(plt.logger, plt.diff,
+                                  layout_matrix = layout_matrix,
+                                  top = grob.title))
   })
 
 }
 
-# A, B, dist, sd
-# tmp <- compute(locations$ppnt_cde[15L])
-# tmp <- lapply(locations$ppnt_cde, compute)
+# compute(logger.names[1])
+# lapply(logger.names, compute)
 
 parallel::setDefaultCluster(parallel::makeCluster(spec = 7L))
-parallel::clusterExport(varlist = c('d', 'd.vec', 'compare', 'read.full', 'locations'))
-results <- parallel::clusterApplyLB(x = locations$ppnt_cde, fun = compute)
+parallel::clusterExport(varlist = c('logger.names', 'round_timestamp', 'read.baro', 'compare'))
+invisible(parallel::clusterApplyLB(x = logger.names, fun = compute))
 parallel::stopCluster(cl = parallel::getDefaultCluster())
 
