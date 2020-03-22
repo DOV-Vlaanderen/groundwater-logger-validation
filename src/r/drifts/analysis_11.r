1c231320c601677fda9430b0f603ff881566d3a7
davorj <davorj@live.com>
2020-03-22 18:25:37 +0000

    added v10 analysis

--- src/r/drifts/analysis_11.r
+++ src/r/drifts/analysis_11.r
@@ -1,4 +1,4 @@
-# KNMI series analysis similar to v04.
+# KNMI series analysis similar to v04 en v10.
 
 logger.names <- grep('barodata/', gwloggeR.data::enumerate(), value = TRUE)
 logger.names <- c(logger.names, 'KNMI_20200312_hourly')
@@ -92,6 +92,41 @@ compute <- function(logger.name, make.plot = TRUE) {
   structure(df.diff, 'logger.name' = logger.name)
 }
 
-compute('KNMI_20200312_hourly')
+result <- compute('KNMI_20200312_hourly')
 
+# ANALYSIS similar to v10
 
+list.quant <- lapply(list(result), function(df) {
+  if (is.null(df)) return(data.table::data.table())
+  df.Q <- df[, .(Q.5 = quantile(PRESSURE_DIFF, 0.5), N.GROUP = .N), by = TIMESTAMP_UTC]
+  df.Q <- df.Q[N.GROUP >= 10, ]
+  wiskers <- boxplot.stats(df.Q[, Q.5])$stats[c(1, 5)]
+  df.Q[, Q.5.centered := Q.5 - mean(wiskers)]
+  df.Q[, FILE := basename(attr(df, 'logger.name'))]
+  df.Q[, N := .N]
+  df.Q[, IQR := IQR(Q.5)]
+  df.Q[, WISK := wiskers[2L] - wiskers[1L]]
+  df.Q[, PERC.95 := quantile(Q.5, 0.975) - quantile(Q.5, 0.025)]
+  df.Q[, SD := sd(Q.5)]
+  df.Q
+})
+
+local(with(list.quant[[1L]], {
+  print(ggplot2::ggplot(mapping = ggplot2::aes(x = TIMESTAMP_UTC)) +
+          ggplot2::geom_line(mapping = ggplot2::aes(y = Q.5), col = 'red', alpha = 0.5) +
+          ggplot2::theme_light() +
+          ggplot2::theme(axis.text.y = ggplot2::element_text(angle = 90, hjust = 0.5)))
+  plot(x = TIMESTAMP_UTC, y = Q.5, pch = '.')
+
+  par(mfrow = c(1, 2))
+  on.exit(par(mfrow = c(1, 1)))
+  hist(Q.5, breaks = 50, main = '')
+  boxplot(Q.5)
+  boxplot.stats(Q.5)$stats
+}))
+
+df.quant <- data.table::rbindlist(list.quant) # 424332 observations, 414692 with df.quant[N.GROUP >= 10, ]
+df.quant[, LABEL := sprintf('%s (#%i) [%.2f WISK]', FILE, N, WISK)]
+df.quant <- df.quant[order(WISK, LABEL), ]
+df.quant[, LABEL := factor(LABEL, levels = unique(LABEL))]
+df.quant[1L,]
