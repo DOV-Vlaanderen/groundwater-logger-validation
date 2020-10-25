82bf76f9efbaca6877a6193f2ecf3ebd045ef1b6
davorj <davorj@live.com>
2020-10-25 21:24:48 +0000

    results.csv added as output

--- src/r/drifts/analysis_32.r
+++ src/r/drifts/analysis_32.r
@@ -9,41 +9,57 @@ df.ref <- gwloggeR.data::read('KNMI_20200312_hourly')$df
 
 ref <- list(list(x = df.ref$PRESSURE_VALUE, timestamps = df.ref$TIMESTAMP_UTC))
 
-local({
-
-  for (f in  logger.names) {
-
-    print(Sys.time())
-    print(basename(f))
-
-    ROOT.PATH <- './drifts/analysis_32/'
-
-    df <- gwloggeR.data::read(f)$df
-    df <- df[!is.na(TIMESTAMP_UTC),]
-    df <- df[!is.na(PRESSURE_VALUE),]
-    data.table::setkey(df, TIMESTAMP_UTC)
-
-    if (nrow(df) == 0L) {
-      print('Skipping...')
-      next()
-    }
-
-    gwloggeR:::test.detect_function(
-      fun = gwloggeR::detect_drift,
-      x = df$PRESSURE_VALUE,
-      timestamps = df$TIMESTAMP_UTC,
-      reference = ref,
-      apriori = gwloggeR::Apriori(data_type = 'air pressure', units = 'cmH2O'),
-      verbose = TRUE,
-      plot = TRUE,
-      title = paste0(basename(f), ' - v0.01'),
-      RESULT.PATH = paste0(ROOT.PATH, basename(f), '.result'),
-      ATTRIB.PATH = paste0(ROOT.PATH, basename(f), '.attribs'),
-      IMG.PATH = paste0(ROOT.PATH, basename(f), '.png')
-    )
-
-  }
+save.output <- function(f) {
 
   print(Sys.time())
+  print(basename(f))
 
-})
+  ROOT.PATH <- './drifts/analysis_32/'
+
+  df <- gwloggeR.data::read(f)$df
+  df <- df[!is.na(TIMESTAMP_UTC),]
+  df <- df[!is.na(PRESSURE_VALUE),]
+  data.table::setkey(df, TIMESTAMP_UTC)
+
+  if (nrow(df) == 0L) {
+    print('Skipping...')
+    return()
+  }
+
+  gwloggeR:::test.detect_function(
+    fun = gwloggeR::detect_drift,
+    x = df$PRESSURE_VALUE,
+    timestamps = df$TIMESTAMP_UTC,
+    reference = ref,
+    apriori = gwloggeR::Apriori(data_type = 'air pressure', units = 'cmH2O'),
+    verbose = TRUE,
+    plot = TRUE,
+    title = paste0(basename(f), ' - v0.01'),
+    significance = 1/1000,
+    RESULT.PATH = paste0(ROOT.PATH, basename(f), '.result'),
+    ATTRIB.PATH = paste0(ROOT.PATH, basename(f), '.attribs'),
+    IMG.PATH = paste0(ROOT.PATH, basename(f), '.png')
+  )
+
+  gwloggeR::detect_drift(
+    x = df$PRESSURE_VALUE,
+    timestamps = df$TIMESTAMP_UTC,
+    reference = ref,
+    plot = FALSE,
+    verbose = TRUE,
+    significance = 1
+  )
+
+}
+
+results <- sapply(logger.names, save.output, USE.NAMES = TRUE, simplify = FALSE)
+
+results.df <- data.table::rbindlist(lapply(names(results), function(ln) {
+  a <- attributes(results[[ln]])
+  a[['name']] <- basename(ln)
+  a[['year.seasonality']] <- NULL
+  a[['class']] <- NULL
+  a
+}), use.names = TRUE, fill = TRUE)
+
+write.csv(results.df, file = './drifts/analysis_32/results.csv')
